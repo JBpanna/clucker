1e9d323659131a3e3f148f3ac0cd7a8a6383907d
JBpanna <jbworth@Jodies-MacBook-Pro.local>
2014-07-21 00:25:53 -0400

    enter validates and sends

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -37,7 +37,36 @@ $(document).ready(function(){
     			 console.log(data)
                 });
             };
+        
 	    }); 
+
+        $('.chick_login_center').children().keypress(function(e){ 
+            var firstName = $('input:text[name=chick_first_name]').val();
+            var lastName = $('input:text[name=chick_last_name]').val();
+            var loginEmail = $('input:text[name=chick_login_email]').val();
+            var emailReg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
+            if (e.keyCode==13){
+            if ((firstName==null || typeof(firstName)=="undefined" || firstName<1) ||
+                (lastName==null || typeof(lastName)=="undefined" || lastName<1))
+                {$('.chicken_output').text("We need your name for our spam files.");
+                return false;   
+                };
+            if ( !emailReg.test(loginEmail))
+                {$('.chicken_output').append("We need your e-mail for our spam files.");
+                return false;
+                } else 
+                
+            
+                {$.ajax({
+                  url: '/main/api',
+                  data: { login: {firstNameLogin: firstName, lastNameLogin: lastName, EMailLogin: loginEmail}},
+                  type: 'POST'
+               }).done(function(data){
+                 console.log(data)
+                });
+            };
+        };
+        }); 
     
 });
 	
